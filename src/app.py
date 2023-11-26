a1ba9c025c5bd08fb65e38a36e729d5862fce022
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-11-26 00:30:34 +0000

    Codear Delete en app

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Films, Favorites
+from json 
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +38,106 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    users = User.query.all()
+    if len(users) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_users = [x.serialize() for x in users]
+    return serialized_users, 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": f"user with id {user_id} not found"}), 404
+    serialized_user = user.serialize()
+    return serialized_user, 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_one_user():
+    body = json.loads(request.data)
+    new_user = User(
+        name = body["name"],
+        email = body["email"],
+        password = body["password"],
+        is_active = True
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created succesfull", "user_added": new_user }), 200
+
+
+@app.route('/character', methods=['GET'])
+def get_all_character():
+    character = Character.query.all()
+    if len(character) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_character = list(map(lambda x: x.serialize(), character))
+    return serialized_character, 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_one_character(character_id):
+    character = Character.query.get(character_id)
+    if character is None:
+        return jsonify({"msg": f"Character with id {character_id} not found"}), 404
+    serialized_character = character.serialize()
+    return serialized_character, 200
+
+@app.route('/planet', methods=['GET'])
+def get_all_planet():
+    planet = Planet.query.all()
+    if len(planet) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_planet = list(map(lambda x: x.serialize(), planet))    
+    return serialized_planet, 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        return jsonify({"msg": f"Planet with id {planet_id} not found"}), 404
+    serialized_planet = planet.serialize()
+    return serialized_planet, 200
+
+@app.route('/films', methods=['GET'])
+def get_all_films():
+    films = Films.query.all()
+    if len(films) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_films = list(map(lambda x: x.serialize(), films))
+    return serialized_films, 200
+
+@app.route('/films/<int:films_id>', methods=['GET'])
+def get_one_films(films_id):
+    films = Films.query.get(films_id)
+    if films is None:
+        return jsonify({"msg": f"Films with id {films_id} not found"}), 404
+    serialized_films = films.serialize()
+    return serialized_films, 200
+
+@app.route('/user/<int:user_id>/favorites', methods=['GET'])
+def get_favorites(user_id):
+    favorites = Favorites.query.filter_by(user_id = user_id).all()
+    if len(favorites) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_favorites = list(map(lambda x: x.serialize(), favorites))
+    return serialized_favorites, 200
+
+@app.route('/user', methods=['POST'])
+def add_favorites():
+    body = request.json
+    new_favorites = Favorites(
+        user_id = body["user_id"],
+        planet_id = body["planet_id"],
+        character_id = body["character_id"],
+        films_id = body["film_id"]
+    )
+    if new_favorites.planet_id is None and new_favorites.character_id is None and new_favorites.films_id is None:
+        return jsonify({"msg": "Sos un boludo"}), 400
+    db.session.add(new_favorites)
+    db.session.commit()
+    return jsonify({"msg": "Sos un capo", "add_favorite": new_favorites}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,107 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)       
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref= "user")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,                        
+            "email": self.email,
+            "password": self.password,            
+            "is_active": self.is_active            
+            # do not serialize the password, its a security breach
+        }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref= "character")
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color
+            # do not serialize the password, its a security breach
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gravity = db.Column(db.String(80), unique=False, nullable=False)
+    clima = db.Column(db.String(80), unique=False, nullable=False)
+    poblation = db.Column(db.String(80), unique=False, nullable=False)
+    rotation_period = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref= "planet")
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gravity": self.gravity,
+            "clima": self.clima,
+            "poblation": self.poblation,
+            "rotation_period": self.rotation_period
+            # do not serialize the password, its a security breach
+        }
+
+class Films(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    created = db.Column(db.String(80), unique=False, nullable=False)
+    edited = db.Column(db.String(80), unique=False, nullable=False)
+    producer = db.Column(db.String(80), unique=False, nullable=False)
+    title = db.Column(db.String(80), unique=False, nullable=False)
+    director = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref= "films")
+
+    def __repr__(self):
+        return '<Films %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created": self.created,
+            "edited": self.edited,
+            "producer": self.producer,
+            "title": self.title,
+            "director": self.director,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorites(db.Model):
+    id= db.Column(db.Integer, primary_key="true")
+    user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    planet_id= db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=False)
+    character_id= db.Column(db.Integer, db.ForeignKey("character.id"), nullable=False)
+    films_id= db.Column(db.Integer, db.ForeignKey("films.id"), nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+            "films_id": self.films_id            
             # do not serialize the password, its a security breach
         }
